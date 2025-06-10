10d4cff86c4a3617c26d0c4dd03609ec0e2b896a
mohangavade18 <128909795+MohanGavade@users.noreply.github.com>
2025-06-10 10:25:07 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,34 +1,61 @@
-from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-
 import streamlit as st
 import os
+from langchain_groq import ChatGroq
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.embeddings import OllamaEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+import time
+
 from dotenv import load_dotenv
+load_dotenv()
 
-os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
-## Langmith tracking
-os.environ["LANGCHAIN_TRACING_V2"]="true"
-os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+## load the Groq API key
+groq_api_key=os.environ['GROQ_API_KEY']
 
-## Prompt Template
+if "vector" not in st.session_state:
+    st.session_state.embeddings=OllamaEmbeddings()
+    st.session_state.loader=WebBaseLoader("https://docs.smith.langchain.com/")
+    st.session_state.docs=st.session_state.loader.load()
 
-prompt=ChatPromptTemplate.from_messages(
-    [
-        ("system","You are a helpful assistant. Please response to the user queries"),
-        ("user","Question:{question}")
-    ]
+    st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
+    st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:50])
+    st.session_state.vectors=FAISS.from_documents(st.session_state.final_documents,st.session_state.embeddings)
+
+st.title("ChatGroq Demo")
+llm=ChatGroq(groq_api_key=groq_api_key,
+             model_name="mixtral-8x7b-32768")
+
+prompt=ChatPromptTemplate.from_template(
+"""
+Answer the questions based on the provided context only.
+Please provide the most accurate response based on the question
+<context>
+{context}
+<context>
+Questions:{input}
+
+"""
 )
+document_chain = create_stuff_documents_chain(llm, prompt)
+retriever = st.session_state.vectors.as_retriever()
+retrieval_chain = create_retrieval_chain(retriever, document_chain)
 
-## streamlit framework
+prompt=st.text_input("Input you prompt here")
 
-st.title('Langchain Demo With OPENAI API')
-input_text=st.text_input("Search the topic u want")
+if prompt:
+    start=time.process_time()
+    response=retrieval_chain.invoke({"input":prompt})
+    print("Response time :",time.process_time()-start)
+    st.write(response['answer'])
 
-# openAI LLm 
-llm=ChatOpenAI(model="gpt-3.5-turbo")
-output_parser=StrOutputParser()
-chain=prompt|llm|output_parser
-
-if input_text:
-    st.write(chain.invoke({'question':input_text}))
+    # With a streamlit expander
+    with st.expander("Document Similarity Search"):
+        # Find the relevant chunks
+        for i, doc in enumerate(response["context"]):
+            st.write(doc.page_content)
+            st.write("--------------------------------")
+    
