1d11c8da7d4180ddbd4888c7c44d5001885c47f8
mohangavade18 <128909795+MohanGavade@users.noreply.github.com>
2025-06-10 10:26:19 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,54 +1,69 @@
 import streamlit as st
 import os
 from langchain_groq import ChatGroq
-from langchain_community.document_loaders import WebBaseLoader
-from langchain.embeddings import OllamaEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
-from langchain_community.vectorstores import FAISS
-import time
+from langchain_objectbox.vectorstores import ObjectBox
+from langchain_community.document_loaders import PyPDFDirectoryLoader
 
 from dotenv import load_dotenv
 load_dotenv()
 
-## load the Groq API key
-groq_api_key=os.environ['GROQ_API_KEY']
+## load the Groq And OpenAI Api Key
+os.environ['OPEN_API_KEY']=os.getenv("OPENAI_API_KEY")
+groq_api_key=os.getenv('GROQ_API_KEY')
 
-if "vector" not in st.session_state:
-    st.session_state.embeddings=OllamaEmbeddings()
-    st.session_state.loader=WebBaseLoader("https://docs.smith.langchain.com/")
-    st.session_state.docs=st.session_state.loader.load()
+st.title("Objectbox VectorstoreDB With Llama3 Demo")
 
-    st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
-    st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:50])
-    st.session_state.vectors=FAISS.from_documents(st.session_state.final_documents,st.session_state.embeddings)
-
-st.title("ChatGroq Demo")
 llm=ChatGroq(groq_api_key=groq_api_key,
-             model_name="mixtral-8x7b-32768")
+             model_name="Llama3-8b-8192")
 
 prompt=ChatPromptTemplate.from_template(
-"""
-Answer the questions based on the provided context only.
-Please provide the most accurate response based on the question
-<context>
-{context}
-<context>
-Questions:{input}
+    """
+    Answer the questions based on the provided context only.
+    Please provide the most accurate response based on the question
+    <context>
+    {context}
+    <context>
+    Questions:{input}
 
-"""
+    """
+
 )
-document_chain = create_stuff_documents_chain(llm, prompt)
-retriever = st.session_state.vectors.as_retriever()
-retrieval_chain = create_retrieval_chain(retriever, document_chain)
 
-prompt=st.text_input("Input you prompt here")
 
-if prompt:
+## Vector Enbedding and Objectbox Vectorstore db
+
+def vector_embedding():
+
+    if "vectors" not in st.session_state:
+        st.session_state.embeddings=OpenAIEmbeddings()
+        st.session_state.loader=PyPDFDirectoryLoader("./us_census") ## Data Ingestion
+        st.session_state.docs=st.session_state.loader.load() ## Documents Loading
+        st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
+        st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:20])
+        st.session_state.vectors=ObjectBox.from_documents(st.session_state.final_documents,st.session_state.embeddings,embedding_dimensions=768)
+
+
+input_prompt=st.text_input("Enter Your Question From Documents")
+
+if st.button("Documents Embedding"):
+    vector_embedding()
+    st.write("ObjectBox Database is ready")
+
+import time
+
+if input_prompt:
+    document_chain=create_stuff_documents_chain(llm,prompt)
+    retriever=st.session_state.vectors.as_retriever()
+    retrieval_chain=create_retrieval_chain(retriever,document_chain)
     start=time.process_time()
-    response=retrieval_chain.invoke({"input":prompt})
+
+    response=retrieval_chain.invoke({'input':input_prompt})
+
     print("Response time :",time.process_time()-start)
     st.write(response['answer'])
 
@@ -58,4 +73,14 @@ if prompt:
         for i, doc in enumerate(response["context"]):
             st.write(doc.page_content)
             st.write("--------------------------------")
-    
+
+
+
+
+
+
+
+
+
+
+
